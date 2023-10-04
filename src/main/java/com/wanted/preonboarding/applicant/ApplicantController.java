2e6c106183484d64d9cbfed6fba771e59ad64fe0
ffolabear <ffolabear@gmail.com>
2023-10-04 18:25:04 +0900

    feat: Add Applicant related service method and dto

--- src/main/java/com/wanted/preonboarding/applicant/ApplicantController.java
+++ src/main/java/com/wanted/preonboarding/applicant/ApplicantController.java
@@ -1,14 +1,19 @@
 package com.wanted.preonboarding.applicant;
 
 import com.wanted.preonboarding.common.CommonResponse;
+import com.wanted.preonboarding.recruit.dto.ApplicantApplyingDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController("/applicant")
 public class ApplicantController {
+
+    private final ApplicantService applicantService;
 
     //요구사항 5번 - 채용공고 상세내용 조회
     @GetMapping("/{recruitmentId}")
@@ -18,7 +23,7 @@ public class ApplicantController {
 
     //요구사항 6번 - 채용공고 지원
     @PostMapping("/{recruitmentId}")
-    public ResponseEntity<CommonResponse> applyRecruitment(@PathVariable Long recruitmentId) {
+    public ResponseEntity<CommonResponse> postApplyingRecruitment(@PathVariable Long recruitmentId, ApplicantApplyingDto applicantApplyingDto) {
         return null;
     }
 
---
+++ src/main/java/com/wanted/preonboarding/applicant/ApplicantService.java
@@ -0,0 +1,19 @@
+package com.wanted.preonboarding.applicant;
+
+import com.wanted.preonboarding.recruit.dto.ApplicantApplyingDto;
+import com.wanted.preonboarding.recruit.dto.RecruitApplyResponseDto;
+import com.wanted.preonboarding.recruit.dto.RecruitDetailResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicantService {
+
+    public RecruitDetailResponseDto findRecruitmentDetail(Long recruitmentId) {
+        return null;
+    }
+
+    public RecruitApplyResponseDto applyingRecruitment(Long recruitmentId, ApplicantApplyingDto applicantApplyingDto) {
+        return null;
+    }
+
+}
