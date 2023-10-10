50b454e38ae90409ac0c5e186e53a952482624b1
ffolabear <ffolabear@gmail.com>
2023-10-10 22:40:48 +0900

    feat: Add applicant finding recruitment logic

--- src/main/java/com/wanted/preonboarding/applicant/ApplicantService.java
+++ src/main/java/com/wanted/preonboarding/applicant/ApplicantService.java
@@ -3,10 +3,32 @@ package com.wanted.preonboarding.applicant;
 import com.wanted.preonboarding.applicant.dto.ApplicantApplyingDto;
 import com.wanted.preonboarding.recruitment.dto.ApplyRecruitmentResponseDto;
 import com.wanted.preonboarding.recruitment.dto.RecruitmentDetailResponseDto;
+import com.wanted.preonboarding.recruitment.dto.RecruitmentResponseDto;
+import com.wanted.preonboarding.recruitment.dto.RecruitmentSearchRequestDto;
+import com.wanted.preonboarding.recruitment.entity.Recruitment;
+import com.wanted.preonboarding.recruitment.repository.RecruitmentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class ApplicantService {
+
+    private RecruitmentRepository recruitmentRepository;
+
+    public List<RecruitmentResponseDto> findAllRecruitment() {
+        List<Recruitment> recruitments = recruitmentRepository.findAllByOrderByRegDateDesc();
+        return recruitments.stream()
+                .map(recruitment -> recruitment.toRecruitment(recruitment))
+                .collect(Collectors.toList());
+    }
+
+    public RecruitmentResponseDto searchRecruitment(RecruitmentSearchRequestDto recruitSearchRequestDto) {
+        return null;
+    }
 
     public RecruitmentDetailResponseDto findRecruitmentDetail(Long recruitmentId) {
         return null;
