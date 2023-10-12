b204e0cecb849ba01f79bf34ad7345efd715a0b8
ffolabear <ffolabear@gmail.com>
2023-10-12 22:51:20 +0900

    feat: Add recruitment error handling class

---
+++ src/main/java/com/wanted/preonboarding/common/exception/RecruitmentException.java
@@ -0,0 +1,10 @@
+package com.wanted.preonboarding.common.exception;
+
+import com.wanted.preonboarding.common.code.Code;
+
+public class RecruitmentException extends DefaultException{
+
+    public RecruitmentException(Code code) {
+        super(code);
+    }
+}
