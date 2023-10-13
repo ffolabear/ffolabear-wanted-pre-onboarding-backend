99b7f405657e92c0df957305d799a4f80fcd431c
ffolabear <ffolabear@gmail.com>
2023-10-13 22:42:45 +0900

    feat: Add applicant controller logic

--- src/main/java/com/wanted/preonboarding/applicant/ApplicantController.java
+++ src/main/java/com/wanted/preonboarding/applicant/ApplicantController.java
@@ -2,6 +2,9 @@ package com.wanted.preonboarding.applicant;
 
 import com.wanted.preonboarding.applicant.dto.ApplicantApplyingDto;
 import com.wanted.preonboarding.common.CommonResponse;
+import com.wanted.preonboarding.common.code.CommonCode;
+import com.wanted.preonboarding.recruitment.dto.RecruitmentDetailResponseDto;
+import com.wanted.preonboarding.recruitment.dto.RecruitmentResponseDto;
 import com.wanted.preonboarding.recruitment.dto.RecruitmentSearchRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,33 +13,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController("/applicant")
 public class ApplicantController {
 
     private final ApplicantService applicantService;
 
-    @GetMapping
+    @GetMapping("/recruitmentList")
     public ResponseEntity<CommonResponse> getAllRecruitment() {
-        return null;
+        List<RecruitmentResponseDto> allRecruitment = applicantService.findAllRecruitment();
+        return ResponseEntity.ok(CommonResponse.toResponse(CommonCode.OK, allRecruitment));
     }
 
     //요구사항 4-2번 - 채용공고 검색
     @GetMapping("/search")
     public ResponseEntity<CommonResponse> getSearchedRecruitment(RecruitmentSearchRequestDto recruitSearchRequestDto) {
-        return null;
+        List<RecruitmentResponseDto> searchingRecruitment = applicantService.searchRecruitment(recruitSearchRequestDto);
+        return ResponseEntity.ok(CommonResponse.toResponse(CommonCode.OK, searchingRecruitment));
     }
 
     //요구사항 5번 - 채용공고 상세내용 조회
     @GetMapping("/{recruitmentId}")
     public ResponseEntity<CommonResponse> getRecruitmentDetail(@PathVariable Long recruitmentId) {
-        return null;
+        RecruitmentDetailResponseDto recruitmentDetail = applicantService.findRecruitmentDetail(recruitmentId);
+        return ResponseEntity.ok(CommonResponse.toResponse(CommonCode.OK, recruitmentDetail));
     }
 
     //요구사항 6번 - 채용공고 지원
     @PostMapping("/{recruitmentId}")
     public ResponseEntity<CommonResponse> postApplyingRecruitment(@PathVariable Long recruitmentId, ApplicantApplyingDto applicantApplyingDto) {
-        return null;
+        applicantService.applyingRecruitment(recruitmentId,applicantApplyingDto);
+        return ResponseEntity.ok(CommonResponse.toResponse(CommonCode.OK));
     }
 
 }
