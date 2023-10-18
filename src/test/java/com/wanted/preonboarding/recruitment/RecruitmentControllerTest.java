d5050e33a40042a6f6dbe47a3ba4dc7ab7ff24ad
ffolabear <ffolabear@gmail.com>
2023-10-18 22:10:37 +0900

    refactor: Add access modifier at RecruitmentControllerTest

--- src/test/java/com/wanted/preonboarding/recruitment/RecruitmentControllerTest.java
+++ src/test/java/com/wanted/preonboarding/recruitment/RecruitmentControllerTest.java
@@ -16,23 +16,25 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("채용공고 Controller 테스트")
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 class RecruitmentControllerTest {
 
     @Autowired
-    ObjectMapper mapper;
+    private ObjectMapper mapper;
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
     @Autowired
-    CompanyRepository companyRepository;
+    private CompanyRepository companyRepository;
     @Autowired
-    RecruitmentRepository recruitmentRepository;
+    private RecruitmentRepository recruitmentRepository;
     @Autowired
     private TestDataInit testDataInit;
 
