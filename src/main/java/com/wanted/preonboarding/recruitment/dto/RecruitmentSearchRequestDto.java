5f4ad7063a3d3749b3bd515606041d0f31390e7f
ffolabear <ffolabear@gmail.com>
2023-10-18 21:19:21 +0900

    feat: Modify annotations in dto classes

--- src/main/java/com/wanted/preonboarding/recruitment/dto/RecruitmentSearchRequestDto.java
+++ src/main/java/com/wanted/preonboarding/recruitment/dto/RecruitmentSearchRequestDto.java
@@ -1,7 +1,14 @@
 package com.wanted.preonboarding.recruitment.dto;
 
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@AllArgsConstructor
+@Builder
 public class RecruitmentSearchRequestDto {
 
     private String position;
